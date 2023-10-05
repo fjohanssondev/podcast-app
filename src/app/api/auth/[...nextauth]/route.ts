3b4d45eadb7c690bd52e809184dd6ff69fab6629
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-10-05 20:09:20 +0200

    Added auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,8 +7,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
     })
   ]
 }
