2727457e00e596392fa40f1a051b92eb96347dd0
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-10-07 23:33:33 +0200

    Updated layout

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,12 @@
+import { getCurrentSession } from '@lib/getCurrentSession'
+
+export default async function Home() {
+
+  const session = await getCurrentSession()
+
+  return (
+    <main className="flex flex-col items-center p-24">
+      <h1>{JSON.stringify(session?.user) || "No current user"}</h1>
+    </main>
+  )
+}
