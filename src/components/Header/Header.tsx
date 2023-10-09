392fc22c4d64a01a28a24ea2bc8728ee0d9f4283
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-10-09 22:39:25 +0200

    chore: removed the profile dropdown and made it its own component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,18 @@
-import ThemeSwitcher from '@/components/ThemeSwitcher'
-import { getCurrentSession } from '@lib/getCurrentSession'
-import Image from 'next/image'
 import Search from '@components/Search';
-import { ChevronIcon } from '@/icons/ChevronIcon';
+import ThemeSwitcher from '@/components/ThemeSwitcher'
+import ProfileDropdown from '@components/ProfileDropdown';
 
-const Header = async () => {
-
-  const session = await getCurrentSession();
+const Header = () => {
 
   return (
-    <header className="bg-white border-b border-b-slate-200 dark:bg-transparent dark:border-0 py-6">
+    <header className="bg-white border-b border-b-slate-200 dark:bg-transparent dark:border-0 py-3">
       <div className='flex items-center justify-between min-w-full container px-12'>
         <span aria-hidden="true"></span>
         <Search />
         <div className='flex items-center gap-8'>
           <ThemeSwitcher />
-          <div className='flex items-center gap-3'>
-            <Image className='rounded-full' src={session?.user.image as string || session?.user.name?.charAt(0) as string} alt={session?.user.name as string} width={40} height={40} />
-            <span className='text-sm'>{session?.user.name}</span>
-            <ChevronIcon direction='right' />
+          <div className='relative'>
+            <ProfileDropdown />
           </div>
         </div>
       </div>
