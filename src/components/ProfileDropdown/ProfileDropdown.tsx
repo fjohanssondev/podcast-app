28ab2c90d3d40a796cad407616b10120e90b6d89
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-11 22:37:37 +0200

    fix: updated styling and a11y
    Updated some styling and added the aria-haspopup attribute to the menu that is popping up.

--- src/components/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -19,17 +19,17 @@ const ProfileDropdown = () => {
 
   return (
    <>
-    <button ref={ref} onClick={() => setIsOpen(prev => !prev)} className='flex items-center gap-2'>
+    <button aria-haspopup="true" ref={ref} onClick={() => setIsOpen(prev => !prev)} className='flex items-center gap-2'>
       <Avatar size="sm" name={session?.user.name ?? ''} image={session?.user.image ?? ''} />
       <span className='text-sm font-medium'>{session?.user.name}</span>
       <ChevronIcon direction='down' color='black' />
     </button>
     {isOpen && (
-      <div className='absolute border border-slate-200 top-12 right-0 w-48 bg-white rounded shadow-sm py-2'>
-        <Link href="/profile" className='block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900'>View Profile</Link>
-        <Link href="/settings" className='block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900'>Settings</Link>
-        <div className='border-b my-2 border-gray-100'></div>
-        <Link href='/api/auth/signout' className='block px-4 py-2 text-sm font-medium text-red-500 hover:bg-red-100'>Sign out</Link>
+      <div role="menu" className='absolute border border-slate-200 top-12 right-0 w-48 bg-white dark:bg-gray-800 dark:border-gray-700 rounded shadow-sm py-2'>
+        <Link href="/profile" className='block px-4 py-2 text-sm font-medium text-gray-700 dark:dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-900'>View Profile</Link>
+        <Link href="/settings" className='block px-4 py-2 text-sm font-medium text-gray-700 dark:dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-900'>Settings</Link>
+        <div className='border-b my-2 border-gray-100 dark:border-gray-600'></div>
+        <Link href='/api/auth/signout' className='block px-4 py-2 text-sm font-medium text-red-500 hover:bg-red-100 dark:text-red-400 dark:hover:bg-gray-900 dark:hover:text-white'>Sign out</Link>
       </div>
     )}
    </>
