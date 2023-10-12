33072a51a140f9e083484ad8636a82e61e0053c8
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-12 22:04:06 +0200

    fix: added accessibility so the user automatically gets focus when the dialog opens up. The user can't tab itself outside either

--- src/components/Dialog/DialogContent.tsx
+++ src/components/Dialog/DialogContent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { DialogContext } from './DialogRoot'
 import { CloseIcon } from '@/icons/CloseIcon'
 
@@ -13,13 +13,60 @@ interface DialogContentProps {
 const DialogContent = (props: DialogContentProps) => {
 
   const { isOpen, closeDialog } = useContext(DialogContext)
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      const modalElement = modalRef.current;
+      if (!modalElement) return;
+
+      const focusableElements = modalElement.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+      );
+      const firstElement = focusableElements[0] as HTMLElement;
+      const lastElement = focusableElements[focusableElements.length - 1] as HTMLElement;
+
+      if (firstElement){
+        firstElement.focus();
+      }
+
+      const handleTabKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Tab") {
+          if (event.shiftKey && document.activeElement === firstElement) {
+            event.preventDefault();
+            lastElement.focus();
+          } else if (
+            !event.shiftKey &&
+            document.activeElement === lastElement
+          ) {
+            event.preventDefault();
+            firstElement.focus();
+          }
+        }
+      };
+
+      const handleEscapeKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          closeDialog();
+        }
+      };
+
+      modalElement.addEventListener("keydown", handleTabKeyPress);
+      modalElement.addEventListener("keydown", handleEscapeKeyPress);
+
+      return () => {
+        modalElement.removeEventListener("keydown", handleTabKeyPress);
+        modalElement.removeEventListener("keydown", handleEscapeKeyPress);
+      };
+    }
+  }, [isOpen, closeDialog]);
 
   return (
-    <dialog className="relative z-10" role="dialog" aria-modal="true" open={isOpen}>
+    <dialog className="relative z-10" role="dialog" aria-modal="true" ref={modalRef} open={isOpen}>
       <div className="fixed inset-0 bg-black/30 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
       <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-      <div className="flex flex-col relative p-4 transform overflow-hidden rounded-sm bg-white dark:bg-gray-700 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+      <div className="flex flex-col relative py-4 px-6 transform overflow-hidden rounded-sm bg-white dark:bg-gray-700 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
       <button className='self-end' onClick={closeDialog}>
         <CloseIcon color='black' />
       </button>
--- src/components/Dialog/DialogPortal.tsx
+++ src/components/Dialog/DialogPortal.tsx
@@ -1,12 +1,25 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 const DialogPortal = ({ children }: { children: React.ReactNode }) => {
-  return createPortal(
-    children,
-    document.body
-  )
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const newContainer = document.createElement('div');
+    document.body.appendChild(newContainer);
+
+    setContainer(newContainer);
+
+    return () => {
+      document.body.removeChild(newContainer);
+    };
+  }, [])
+
+  if (!container) return null;
+  
+  return createPortal(children, container);
 }
 
 export { DialogPortal }
