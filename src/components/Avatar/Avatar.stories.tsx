08248c8f585eacc6c81a2d35cae3b71eccbfdbea
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-12 12:35:08 +0200

    docs: added story for the Avatar component

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Avatar from './Avatar';
+
+const meta: Meta<typeof Avatar> = {
+  component: Avatar,
+};
+
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const Primary: Story = {
+  args: {
+    image: 'https://avatars.githubusercontent.com/u/4060187?v=4',
+    name: 'John Doe',
+    size: 'sm',
+    rounded: true,
+  },
+};
