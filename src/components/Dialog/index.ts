35e85e63944349fde2df5bb31e9fd6ad2941795a
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-12 21:05:45 +0200

    feat: added export for the dialog component

---
+++ src/components/Dialog/index.ts
@@ -0,0 +1,11 @@
+import { DialogRoot } from './DialogRoot';
+import { DialogTrigger } from './DialogTrigger';
+import { DialogPortal } from './DialogPortal';
+import { DialogContent } from './DialogContent';
+
+export const Dialog = {
+  Root: DialogRoot,
+  Trigger: DialogTrigger,
+  Portal: DialogPortal,
+  Content: DialogContent,
+}
