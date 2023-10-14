c7648041ba0324c3c0ebce64676b98e2cd96d743
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-14 16:11:17 +0200

    fix: added the correct path to link to the signed in users profile

--- src/components/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -26,7 +26,7 @@ const ProfileDropdown = () => {
     </button>
     {isOpen && (
       <div role="menu" className='absolute border border-slate-200 top-12 right-0 w-48 bg-white dark:bg-gray-800 dark:border-gray-700 rounded shadow-sm py-2'>
-        <Link href="/profile" className='block px-4 py-2 text-sm font-medium text-gray-700 dark:dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-900'>View Profile</Link>
+        <Link href={`/profile/${session?.user.id}`} className='block px-4 py-2 text-sm font-medium text-gray-700 dark:dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-900'>View Profile</Link>
         <Link href="/settings" className='block px-4 py-2 text-sm font-medium text-gray-700 dark:dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-900'>Settings</Link>
         <div className='border-b my-2 border-gray-100 dark:border-gray-600'></div>
         <Link href='/api/auth/signout' className='block px-4 py-2 text-sm font-medium text-red-500 hover:bg-red-100 dark:text-red-400 dark:hover:bg-gray-900 dark:hover:text-white'>Sign out</Link>
