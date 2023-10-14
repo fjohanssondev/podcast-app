72ed6a9364fb8db6cbd32c75588ed8216c235e5a
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-15 01:15:09 +0200

    feat: added render of both the followers and following

--- src/components/FollowStats/FollowStats.tsx
+++ src/components/FollowStats/FollowStats.tsx
@@ -1,28 +1,54 @@
 import { Dialog } from "@components/Dialog"
+import { User } from "../Profile/ProfileDetails"
+import ListItem from "./ListItem"
+import EmptyList from "./EmptyList"
 
-interface FollowStatsProps {}
+interface FollowStatsProps {
+  following: User[]
+  followers: User[]
+}
 
-const FollowStats = (props: FollowStatsProps) => {
+const FollowStats = ({ following, followers }: FollowStatsProps) => {
 
   return (
     <div className="flex mt-4 gap-4">
       <Dialog.Root>
         <Dialog.Trigger>
-          <button className="text-sm font-semibold">12 Following</button>
+          <button className="text-sm font-semibold">{following.length} Following</button>
         </Dialog.Trigger>
         <Dialog.Portal>
           <Dialog.Content title="Following" description="A list of people you're following">
-                
+            {following.length === 0 ? (
+              <EmptyList message={"people you're following"} />
+            ) : (
+              <ul className="divide-y divide-gray-200">
+              {following.map((user) => (
+                <li key={user.id}>
+                  <ListItem key={user.id} {...user} />
+                </li>
+              ))}
+            </ul>
+            )}
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
       <Dialog.Root>
         <Dialog.Trigger>
-          <button className="text-sm font-semibold">1402 Followers</button>
+          <button className="text-sm font-semibold">{followers.length} Followers</button>
         </Dialog.Trigger>
         <Dialog.Portal>
           <Dialog.Content title="Followers" description='This is a list of all your current followers'>
-                
+          {followers.length === 0 ? (
+            <EmptyList message={'followers'} />
+          ) : (
+            <ul className="divide-y divide-gray-200">
+            {followers.map((user) => (
+              <li key={user.id}>
+                <ListItem {...user} />
+              </li>
+            ))}
+          </ul>
+          )}
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
