ca69a41271a03d0351a8537069b7b7c6eb9a84b2
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-14 16:10:31 +0200

    fix: extracted the profile stats into it's own component

--- src/app/(app)/profile/[id]/page.tsx
+++ src/app/(app)/profile/[id]/page.tsx
@@ -1,20 +1,13 @@
-import Avatar from '@/components/Avatar'
-import { getCurrentSession } from '@lib/getCurrentSession'
+import ProfileDetails from '@/components/Profile/ProfileDetails'
 
 export default async function Profile({ params }: { params: { id: string } }) {
-
-  const session = await getCurrentSession()
 
   return (
     <section>
       <h1 className='text-4xl font-bold'>Profile - {params.id}</h1>
       <section className='mt-16' aria-label='Profile summary'>
         <div className='flex items-center'>
-          <Avatar image={session?.user.image ?? ''} name={session?.user.name ?? ''} size='lg' />
-          <div className='ml-12'>
-            <h2 className='text-5xl font-bold text-black dark:text-white'>{session?.user.name}</h2>
-            <p className='text-sm max-w-xl mt-6 leading-loose'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          </div>
+          <ProfileDetails id={params.id} />
         </div>
       </section>
       <section aria-label='Playlists'></section>
