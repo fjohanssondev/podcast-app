a7bc7ff621632cf465e29c0f30372fc7118d4353
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-15 01:17:43 +0200

    feat: added data and follow check
    Added data to each dialog-component. Also added a check to see if the current user signed in, isn't the user we are visiting. In that case, don't show the follow-button. Need to add a check here to make sure we aren't already following this person too.

--- src/components/Profile/ProfileDetails.tsx
+++ src/components/Profile/ProfileDetails.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Avatar from '@components/Avatar'
 import FollowStats from '@components/FollowStats';
-
+import { useSession } from 'next-auth/react';
 
 interface ProfileDetailsProps {
   id: string,
@@ -16,9 +16,13 @@ export interface User {
   email: string;
   emailVerified?: Date;
   image: string;
+  following: User[]
+  followedBy: User[]
 }
 
 const ProfileDetails = (props: ProfileDetailsProps) => {
+
+  const { data: session } = useSession()
 
   const { data, isLoading } = useQuery<User>({
     queryKey: ['profile', props.id],
@@ -29,13 +33,22 @@ const ProfileDetails = (props: ProfileDetailsProps) => {
     }
   })
 
+  const isAllowedToFollow = () => {
+    if (session?.user.id !== props.id){
+      return true
+    }
+  }
+
+  const canFollow = isAllowedToFollow()
+
   return (
     <>
       <Avatar image={data?.image ?? ''} name={data?.name ?? ''} size='lg' />
       <div className='ml-16'>
         <h2 className='text-5xl font-bold text-black dark:text-white'>{data?.name}</h2>
         <p className='text-sm font-medium max-w-xl mt-6 leading-loose'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        <FollowStats />
+        <FollowStats following={data?.following ?? []} followers={data?.followedBy ?? []} />
+        {canFollow && <button className='bg-orange-300 px-4 py-2 rounded-sm mt-12'>Follow the user</button>}
       </div>
     </>
   )
