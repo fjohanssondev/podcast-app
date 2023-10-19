183ebb2207b877c23f437ac9aa5fd34e8fc79de8
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-19 17:49:43 +0200

    wip: form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import { Field, Form } from "houseform";
+import { z } from "zod";
+
+export default function FormUI() {
+  return (
+    <Form
+      onSubmit={(values) => {
+        alert("Form was submitted with: " + JSON.stringify(values));
+      }}
+    >
+      {({ isValid, submit }) => (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            submit();
+          }}
+        >
+          <Field
+            name="email"
+            onBlurValidate={z.string().email("This must be an email")}
+            onSubmitValidate={isEmailUnique}
+          >
+            {({ value, setValue, onBlur, errors }) => {
+              return (
+                <div>
+                  <input
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={"Email"}
+                  />
+                  {errors.map((error) => (
+                    <p key={error}>{error}</p>
+                  ))}
+                </div>
+              );
+            }}
+          </Field>
+          <Field<string>
+            name="password"
+            onChangeValidate={z
+              .string()
+              .min(8, "Must be at least 8 characters long")}
+          >
+            {({ value, setValue, onBlur, errors }) => {
+              return (
+                <div>
+                  <input
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={"Password"}
+                    type="password"
+                  />
+                  {errors.map((error) => (
+                    <p key={error}>{error}</p>
+                  ))}
+                </div>
+              );
+            }}
+          </Field>
+          <Field<string>
+            name="confirmpassword"
+            listenTo={["password"]}
+            onChangeValidate={(val, form) => {
+              if (val === form.getFieldValue("password")!.value) {
+                return Promise.resolve(true);
+              } else {
+                return Promise.reject("Passwords must match");
+              }
+            }}
+          >
+            {({ value, setValue, onBlur, errors, isTouched }) => {
+              return (
+                <div>
+                  <input
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={"Password Confirmation"}
+                    type="password"
+                  />
+                  {isTouched &&
+                    errors.map((error) => <p key={error}>{error}</p>)}
+                </div>
+              );
+            }}
+          </Field>
+          <button disabled={!isValid} type="submit">
+            Submit
+          </button>
+        </form>
+      )}
+    </Form>
+  );
+}
+
+// This is simulating a check against a database
+function isEmailUnique(val: string) {
+  return new Promise<boolean>((resolve, reject) => {
+    setTimeout(() => {
+      const isUnique = !val.startsWith("crutchcorn");
+      if (isUnique) {
+        resolve(true);
+      } else {
+        reject("That email is already taken");
+      }
+    }, 20);
+  });
+}
