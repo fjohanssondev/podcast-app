a2e349b25abbcec558e9ed81b0185ba84db080b1
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-19 19:50:49 +0200

    fix: updated the props
    This will be used in all newer icons and when we fix the old ones. The new props allows the component to be more adjustable.

--- src/icons/icons.d.ts
+++ src/icons/icons.d.ts
@@ -3,3 +3,10 @@ import { colors } from "./colors";
 export interface IconProps {
   color?: keyof typeof colors;
 }
+
+export interface NewIconProps {
+  type: 'solid' | 'duotone' | 'line';
+  primaryColor?: string;
+  secondaryColor?: string;
+  size?: 'sm' | 'md' | 'lg';
+}
