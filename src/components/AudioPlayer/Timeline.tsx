9cb16bccfdbe83bbc329673a9d2c88aeef9667d5
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-19 19:52:20 +0200

    feat: added a timeline to the top of the player

--- src/components/AudioPlayer/Timeline.tsx
+++ src/components/AudioPlayer/Timeline.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const Timeline = () => {
-  return (
-    <div>Timeline</div>
-  )
+  return <progress className='appearance-none absolute top-0 w-full bg-gray-100 h-2' value='30' max='100' />
+  
 }
 
 export { Timeline }
