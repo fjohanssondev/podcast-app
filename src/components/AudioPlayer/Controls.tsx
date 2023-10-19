4f3303167ad021689ef74bc8988c32cc8e3fd85f
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-19 19:51:59 +0200

    feat: added play, pause, next and previous icon

--- src/components/AudioPlayer/Controls.tsx
+++ src/components/AudioPlayer/Controls.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client'
+
+import NextIcon from '@/icons/NextIcon'
+import PauseIcon from '@/icons/PauseIcon'
+import PlayIcon from '@/icons/PlayIcon'
+import PreviousIcon from '@/icons/PreviousIcon'
+import React, { useState } from 'react'
 
 const Controls = () => {
-  return
-    <div>Controls</div>
+
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  return (
+    <div className='flex items-center gap-2'>
+      <button aria-label="Play the previous song">
+        <PreviousIcon type="solid" size="md" />
+      </button>
+      <button aria-pressed={isPlaying} aria-label={isPlaying ? "Pause the song" : "Play the song"} onClick={() => setIsPlaying(prev => !prev)}>
+        {isPlaying ? <PauseIcon type="solid" size="lg" /> : <PlayIcon type="solid" size="lg" />}
+      </button>
+      <button aria-label="Play the next song">
+        <NextIcon type="solid" size="md" />
+      </button>
+    </div>
+  )
 
 }
 
