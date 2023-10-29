9d7f926b6980802aa5ea795ecb507e020e4876f1
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-29 23:21:02 +0100

    fix: updates

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -5,6 +5,7 @@ import { AudioInfo } from './AudioInfo'
 import { Controls } from './Controls'
 import { Volume } from './Volume'
 import { Timeline } from './Timeline'
+import { signIn, signOut } from 'next-auth/react'
 
 const AudioPlayer = () => {
   
@@ -13,6 +14,8 @@ const AudioPlayer = () => {
 
   return (
     <div className='relative bg-white pt-8 pb-6'>
+      <button onClick={() => signOut()}>Sign out</button>
+      <button onClick={() => signIn("github")}>Sign in</button>
       <audio ref={audioRef} src='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3' />
       <Timeline audioRef={audioRef} />
       <div className='container mx-auto px-4'>
