8b61f32e47b877d1e9295e33d1683ae2750b1858
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-31 00:56:14 +0100

    fix: removed the old Avatar ✅

--- src/components/FollowStats/ListItem.tsx
+++ src/components/FollowStats/ListItem.tsx
@@ -1,5 +1,5 @@
+import { Avatar } from "@radix-ui/themes";
 import Link from "next/link";
-import Avatar from "@components/Avatar";
 
 interface ListItemProps {
   id: string;
@@ -11,7 +11,7 @@ interface ListItemProps {
 const ListItem = ({ id, name, email, image }: ListItemProps) => {
   return (
     <Link href={`/profile/${id}`} key={id} className="py-4 flex">
-      <Avatar rounded={true} size="sm" image={image} name={name} />
+      <Avatar radius="full" size="3" src={image} fallback={name.charAt(0)} />
       <div className="ml-3">
         <p className="text-sm font-medium text-gray-900">{name}</p>
         <p className="text-xs text-gray-500">{email}</p>
