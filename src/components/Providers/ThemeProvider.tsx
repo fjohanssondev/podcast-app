071db4396a9b8c9f56013519a4230fc20e19d63a
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-30 19:53:40 +0100

    feat: added radix-theme-wrapper

--- src/components/Providers/ThemeProvider.tsx
+++ src/components/Providers/ThemeProvider.tsx
@@ -4,11 +4,11 @@ import { ThemeProvider as ThemeProviderWrapper } from 'next-themes'
 import { Theme } from '@radix-ui/themes';
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-  /* return <ThemeProviderWrapper attribute="class">{children}</ThemeProviderWrapper> */
-
   return (
-    <Theme appearance="dark" accentColor="orange" grayColor="mauve" panelBackground="solid" radius="small">
-      {children}
-    </Theme>
+    <ThemeProviderWrapper attribute="class">
+      <Theme>
+        {children}
+      </Theme>
+    </ThemeProviderWrapper>
   )
 }
