78e9949e1ee3c9f5e3c13a711938fd1dfce60457
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-31 00:53:13 +0100

    fix: removed the old getCurrentSession

--- src/app/(app)/profile/[id]/settings/page.tsx
+++ src/app/(app)/profile/[id]/settings/page.tsx
@@ -1,13 +1,6 @@
-import { getCurrentSession } from "@lib/getCurrentSession"
 import { redirect } from "next/navigation"
 
 export default async function Settings({ params }: { params: { id: string } }) {
-
-  const session = await getCurrentSession()
-
-  if (session?.user.id !== params.id){
-    redirect('/')
-  }
 
   return (
     <section>
