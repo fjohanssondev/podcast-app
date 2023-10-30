a7b50cb56328ba69c34a3abfe53ae1f6601eec3b
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-31 00:47:01 +0100

    fix: updated colors 🎨

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,14 @@
+import { Text } from "@radix-ui/themes"
 
 const Sidebar = () => {
   return (
-    <aside className='bg-gray-100 dark:bg-black h-full px-12'>Sidebar</aside>
+    <aside className='fixed bg-gray-100 dark:bg-dark-primary w-64 h-full py-6 px-12'>
+      <div className="flex flex-col">
+        <div className="text-center">
+          <Text weight={"bold"} size={"6"}><span className="text-orange-800">Son</span>are</Text>
+        </div>
+      </div>
+    </aside>
   )
 }
 
